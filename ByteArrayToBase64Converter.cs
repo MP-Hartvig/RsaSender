e8797acb8ba3cbfc9145bd2e9e5252a2b6eed09f
MP-Hartvig <mip@hartvigconsult.dk>
2023-08-10 13:12:24 +0200

    Removed unnecessary using statements

--- ByteArrayToBase64Converter.cs
+++ ByteArrayToBase64Converter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RsaSender
+﻿namespace RsaSender
 {
     internal class ByteArrayToBase64Converter
     {
